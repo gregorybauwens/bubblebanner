04bdc6557c44664575fdbacf476ffc9e4dc47960
gregorybauwens <gregory.bauwens@gmail.com>
2026-03-26 20:12:45 -0700

    Update Embed.tsx

--- src/pages/Embed.tsx
+++ src/pages/Embed.tsx
@@ -1,13 +1,19 @@
 import { useEffect } from "react";
 import InteractiveHeroBanner, { DEFAULT_CONTROLS } from "@/components/InteractiveHeroBanner";
 
-const HAZE_STOPS = [
-  "#FFD166",
-  "#FF9E64",
-  "#FF6E91",
-  "#D490D4",
-  "#8ABFFF",
-  "#A5F3FC",
+const GRAPE_MIST_III_CONTROLS = {
+  ...DEFAULT_CONTROLS,
+  hoverStrength: 1.6,
+  hoverRadius: 0.2,
+};
+
+const GRAPE_MIST_III_STOPS = [
+  "#D06E4D",
+  "#C05454",
+  "#BD6385",
+  "#0BB8C0",
+  "#078FCB",
+  "#7174CA",
 ];
 
 const Embed = () => {
@@ -23,8 +29,8 @@ const Embed = () => {
     <div className="min-h-screen bg-transparent flex items-center justify-center p-0">
       <div className="w-full max-w-[1312px] mx-auto">
         <InteractiveHeroBanner
-          colorStops={HAZE_STOPS}
-          initialControls={DEFAULT_CONTROLS}
+          colorStops={GRAPE_MIST_III_STOPS}
+          initialControls={GRAPE_MIST_III_CONTROLS}
           persistControls={false}
         />
       </div>
